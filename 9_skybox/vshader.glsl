7919ec3cebe11900571d6f48abdaf7a15688c7e9
channyHuang <349117102@qq.com>
2021-12-09 21:02:19 +0800

    lesson 9

---
+++ 9_skybox/vshader.glsl
@@ -0,0 +1,21 @@
+#version 430 core
+
+in vec3 posVertex;
+in vec3 norVertex;
+in vec2 posTexture;
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+uniform mat3 normal_matrix;
+
+varying vec2 v_texcoord;
+varying vec3 vert;
+varying vec3 vertNormal;
+
+void main(void)
+{
+    vert = posVertex;
+    vertNormal = normal_matrix * norVertex;
+    gl_Position = proj_matrix * mv_matrix * vec4(posVertex, 1.0f);
+    v_texcoord = posTexture;
+}
+
